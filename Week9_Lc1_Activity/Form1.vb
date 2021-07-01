1874025d851bdc647edeb9b6730a15a0f436d72e
intang02 <intnataufik@gmail.com>
2021-07-01 08:15:58 +0800

    Add project files.

---
+++ Week9_Lc1_Activity/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim empFile As System.IO.StreamReader
+        Dim rec, strRec() As String
+        empFile = New System.IO.StreamReader("C:\Users\user\Documents\UTP\FOUNDATION\SEM 3 MAY21\VISUAL PROGRAMMING\w9lc1activity.txt")
+        Do Until empFile.Peek = -1
+            rec = empFile.ReadLine
+            strRec = rec.Split(",")
+            lstEmployeeInfo.Items.Add(strRec(0) & vbTab & strRec(3) & vbTab & strRec(4))
+        Loop
+        empFile.Close()
+    End Sub
+End Class
---
+++ Week9_Lc1_Activity/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Week9_Lc1_Activity")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Week9_Lc1_Activity")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("78cac233-32d4-40f6-86f8-33321c2b6109")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
